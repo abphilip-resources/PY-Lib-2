d79eae19c90231499ed5df099aa8c141ddb89226
allenalvin333 <allenalvin333@gmail.com>
2021-12-11 14:00:35 +0530

    programs

--- Dynamic/2.py
+++ Dynamic/2.py
@@ -1,38 +1,37 @@
-import math
+import math                                             # Import math module
 
-def change_making(denominations, target):
-    cache = {}
+def change(denominations, target):                      # Declare function for Problem 1
+    cache = {}                                          # Initialize cache
 
-    def subproblem(i, t):
-        if (i, t) in cache: return cache[(i, t)]  # memoization
-        val = denominations[i]
-        if val > t:
-            choice_take = math.inf
-        elif val == t:
-            choice_take = 1
-        else:
-            choice_take = 1 + subproblem(i, t - val)
+    def rec(i, t):                                      # Declare inner function
+        if (i, t) in cache: return cache[(i, t)]        # Return if value in cache
+        val = denominations[i]                          # Get value of current coin
+        if val > t: y = math.inf                        # If value > target, chance = infinity
+        elif val == t: y = 1                            # If value == target, chance = 1
+        else: y = 1 + rec(i, t-val)                     # Else, chance = 1 + rec(reduced target)
 
-        if i == 0:
-            choice_leave = math.inf
-        else:
-            choice_leave = subproblem(i - 1, t)
+        if i == 0: n = math.inf                         # If at end of coins, chance = infinity
+        else: n = rec(i-1, t)                           # Else, chance = rec(new denomination)
 
-        optimal = min(choice_take, choice_leave)
-        cache[(i, t)] = optimal
-        return optimal
+        optimal = min(y, n)                             # Optimal is least value of chance
+        cache[(i, t)] = optimal                         # Store optimal value in cache
+        return optimal                                  # Return optimal value
 
-    return subproblem(len(denominations) - 1, target)
+    return rec(len(denominations)-1, target)            # Call inner function and return output
 
-def flowerbox(nutrient_values):
-    a = 0  
-    b = 0 
-    for val in nutrient_values:
-        a, b = b, max(a + val, b)
-    return b
+def flowerbox(val):                                     # Declare function for Problem 2
+    a, b = 0, 0                                         # Initialize a and b
+    for v in val: a, b = b, max(a+v, b)                 # Iterate to find max value
+    return b                                            # Return max value
 
-print(f'flowerbox([3, 10, 3, 1, 2]) = {flowerbox([3, 10, 3, 1, 2])}')
-print(f'flowerbox([9, 10, 9]      ) = {flowerbox([9, 10, 9])}')
-print(
-    'change_making([1, 5, 12, 19], 16) = '
-    f'{change_making([1, 5, 12, 19], 16)}')
+print("\nDenomination Problem: ")                       # Solution 1
+print(f'No. of notes to make $16 using \
+[1, 5, 12, 19] = {change([1, 5, 12, 19], 16)}')         # Scenario A     
+print(f'No. of notes to make $17 using \
+[1, 5, 12, 19] = {change([1, 5, 12, 19], 17)}')         # Scenario B
+
+print("\nFlower Box Problem: ")                         # Solution 2
+print(f'Maximum non consecutive height from \
+[3, 10, 3, 1, 2] = {flowerbox([3, 10, 3, 1, 2])}')      # Scenario A
+print(f'Maximum non consecutive height from \
+[9, 10, 9] = {flowerbox([9, 10, 9])}')                  # Scenario B
